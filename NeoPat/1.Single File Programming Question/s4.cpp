54c40e7d2bf6c4e0d4b5a3af1d6b9641dae31597
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-02-26 23:31:50 +0530

    Create s4.cpp

---
+++ NeoPat/1.Single File Programming Question/s4.cpp
@@ -0,0 +1,17 @@
+// You are using GCC
+#include <iostream>
+using namespace std;
+main()
+{
+    int a;
+    cin>>a;
+    if(a %2 ==0)
+    {
+        cout<<"The given number is even.";
+
+    }
+    else
+    {
+        cout<<"The given number is odd.";
+    }
+}
