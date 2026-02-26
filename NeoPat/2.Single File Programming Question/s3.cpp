9079d963042b4544310005f5e3fe900e162a4b20
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-02-26 23:39:55 +0530

    Create s3.cpp

---
+++ NeoPat/2.Single File Programming Question/s3.cpp
@@ -0,0 +1,16 @@
+// You are using GCC
+#include <iostream>
+using namespace std;
+main()
+{
+    int n1,n2;
+    cin>>n1>>n2;
+    if(n1>n2)
+    {
+        cout<<n1-n2;
+    }
+    else
+    {
+        cout<<n2-n1;
+    }
+}
