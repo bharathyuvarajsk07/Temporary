6d9300a93041e329fb80d7cb5494e83d924b36a4
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-05 23:29:10 +0530

    Create q9.java

---
+++ CODECHEF/1.Basics/q9.java
@@ -0,0 +1,5 @@
+In this task, you are required to write a Java program that displays a message containing both double quotes and single quotes, using escape characters and a custom separator.
+
+You should reproduce the following output exactly:
+
+The programmer exclaimed - "It's amazing how Python handles special characters!"
