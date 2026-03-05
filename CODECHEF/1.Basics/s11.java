7a6e93357330de6b6e28e2df328a4142a83cb3ba
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-05 23:36:53 +0530

    Create s11.java

---
+++ CODECHEF/1.Basics/s11.java
@@ -0,0 +1,7 @@
+ class Codechef{
+    public static void main(String[] args){
+        // Cook your dish here 
+        var d=18;
+        System.out.println("The value stored in variable is "+d);
+    }
+}
