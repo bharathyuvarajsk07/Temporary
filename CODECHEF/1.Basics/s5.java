495e35183e42262c5ef575ab2df9b5c217c0ecff
Bharath Yuvaraj S K <bharathyuvarajsk07@gmail.com>
2026-03-05 23:20:40 +0530

    Create s5.java

---
+++ CODECHEF/1.Basics/s5.java
@@ -0,0 +1,7 @@
+class Codechef {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("My name is John");
+        System.out.println("My favorite color is Blue.My favorite hobby is Reading.");
+    }
+}
